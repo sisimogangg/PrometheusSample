70eff05edd55558b64db9e1790441ffe417083ba
sisimogangg <jaysisimogang@gmail.com>
2018-05-27 15:30:04 +0200

    Added Prometheus Support

--- main.go
+++ main.go
@@ -4,19 +4,44 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 func main() {
+	// Prometheus: Histogram to collect required metrics
+	histogram := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "greeting_seconds",
+		Help:    "Time take to greet someone",
+		Buckets: []float64{1, 2, 5, 6, 10}, //defining small buckets as this app should not take more than 1 sec to respond
+	}, []string{"code"}) // this will partitioned by the HTTP code
+
 	router := mux.NewRouter()
-	router.Handle("/sayhello/{name}", Sayhello())
+	router.Handle("/sayhello/{name}", Sayhello(histogram))
+	router.Handle("/metrics", prometheus.Handler()) //Metrics endpoint for scrapping
+
+	//Registering the defined metric with Prometheus
+	prometheus.Register(histogram)
+
 	log.Fatal(http.ListenAndServe(":8009", router))
 }
 
-func Sayhello() http.HandlerFunc {
+func Sayhello(histogram *prometheus.HistogramVec) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		code := http.StatusBadRequest // if req is not GET
+
+		//monitoring how long it takes to respond
+		start := time.Now()
+		defer r.Body.Close()
+		code := 500
+
+		defer func() {
+			httpDuration := time.Since(start)
+			histogram.WithLabelValues(fmt.Sprintf("%d", code)).Observe(httpDuration.Seconds())
+		}()
+
+		code = http.StatusBadRequest // if req is not GET
 		if r.Method == "GET" {
 			code = http.StatusOK
 			vars := mux.Vars(r)
